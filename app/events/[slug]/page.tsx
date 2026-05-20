595cfdc4c2aa2e2d371f6c26885e7aea59a46d8d
Ranjeet Vishwakarma <ranjeetv.dev@gmail.com>
2026-05-20 16:14:10 +0530

    feat: add event booking component

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,6 +1,46 @@
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+import BookEvent from "@/components/BookEvent";
+import Event, { IEvent } from "@/database/event.model";
+import connectDB from "@/lib/mongodb";
+
+const EventDetailItem = ({
+  icon,
+  alt,
+  label,
+}: {
+  icon: string;
+  alt: string;
+  label: string;
+}) => (
+  <div className="flex-row-gap-2 items-center">
+    <Image src={icon} alt={alt} width={17} height={17} />
+    <p>{label}</p>
+  </div>
+);
+
+const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
+  <div className="agenda">
+    <h2>Agenda</h2>
+
+    <ul>
+      {agendaItems?.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  </div>
+);
+
+const EventTags = ({ tags }: { tags: string[] }) => (
+  <div className="flex flex-row gap-1.5 flex-wrap">
+    {tags?.map((tag) => (
+      <div className="pill" key={tag}>
+        {tag}
+      </div>
+    ))}
+  </div>
+);
 
 const EventDetailsPage = async ({
   params,
@@ -8,14 +48,107 @@ const EventDetailsPage = async ({
   params: Promise<{ slug: string }>;
 }) => {
   const { slug } = await params;
-  const request = await fetch(`${BASE_URL}/api/events/${slug}`);
-  const { event } = await request.json();
+
+  await connectDB();
+  const event = await Event.findOne({ slug }).lean<IEvent>();
 
   if (!event) return notFound();
 
+  const {
+    description,
+    overview,
+    image,
+    location,
+    date,
+    time,
+    mode,
+    audience,
+    agenda,
+    organizer,
+    tags,
+  } = event;
+
+  const bookings = 10;
+
+  const formattedDate = new Date(date).toLocaleDateString("en-IN", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+
   return (
     <section id="event">
-      <h1>Event Details: {slug} </h1>
+      <div className="header">
+        <h1>Event Description</h1>
+        <p>{description}</p>
+      </div>
+
+      <div className="details">
+        {/* Left Side - Event Content */}
+        <div className="content">
+          <Image
+            src={image}
+            alt="Event Banner"
+            width={800}
+            height={800}
+            className="banner"
+          />
+
+          {/* Overview */}
+          <section className="flex-col-gap-2">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+          </section>
+
+          {/* Event Details */}
+          <section className="flex-col-gap-2">
+            <h2>Event Details</h2>
+
+            <EventDetailItem
+              icon="/icons/calendar.svg"
+              alt="calendar"
+              label={formattedDate}
+            />
+            <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
+            <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
+            <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
+            <EventDetailItem
+              icon="/icons/audience.svg"
+              alt="audience"
+              label={audience}
+            />
+          </section>
+
+          {/* Event Agenda */}
+          <EventAgenda agendaItems={agenda} />
+
+          {/* Event organizer */}
+          <section>
+            <h2>About the organizer</h2>
+            <p>{organizer}</p>
+          </section>
+
+          {/* Event tags */}
+          <EventTags tags={tags} />
+        </div>
+
+        {/* Right Side - Booking Form */}
+        <aside className="booking">
+          <div className="signup-card">
+            <h2>Book Your Spot</h2>
+
+            {bookings > 0 ? (
+              <p className="text-sm">
+                Join {bookings} people who have already booked their spot!
+              </p>
+            ) : (
+              <p className="text-sm">Be the first to book your spot!</p>
+            )}
+
+            <BookEvent />
+          </div>
+        </aside>
+      </div>
     </section>
   );
 };
